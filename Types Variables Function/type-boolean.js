7ba41e74492c09fe7f0981b65629eccba0adf82a
Fernanda <fernandakagami@gmail.com>
2022-09-05 13:53:28 -0300

    types

---
+++ Types Variables Function/type-boolean.js
@@ -0,0 +1,20 @@
+const fistNumber = 5;
+const secundNumber = 10;
+
+console.log(fistNumber === secundNumber);
+
+const text1 = "Alura";
+const text2 = "a";
+
+console.log(text1 === text2)
+
+//
+
+const city1 = "belo horizonte";
+const city2 = "Belo Horizonte";
+
+console.log(city1 === city2); // false
+
+const city2LowerCase = city2.toLowerCase();
+
+console.log(city1 === city2LowerCase); // true
---
+++ Types Variables Function/type-number.js
@@ -0,0 +1,40 @@
+// Type number
+
+const myNumber = 3;
+
+const firstNumber = 1;
+const secundNumber = 2;
+
+const mathOperation = firstNumber - secundNumber;
+
+console.log(mathOperation)
+
+// Floating point
+
+const numberFloatingPoint = 3.3;
+const floatingPointWithoutZero = .5;
+
+const newOperation = firstNumber / numberFloatingPoint;
+
+console.log(newOperation)
+
+// Nan -> Not a Number
+
+const alura = "Alura";
+
+console.log(alura * firstNumber)
+
+// PI
+
+const pi = Math.PI
+
+console.log(pi)
+
+// Zero Division
+
+var a = 10;
+var b = 0;
+
+console.log(a/b)
+
+
